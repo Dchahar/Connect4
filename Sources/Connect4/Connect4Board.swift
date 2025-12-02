0a45334f29188bae7be8c46f2dc4e023d57023be
Dheeraj Chahar <dheeraj.chahar@ns.nl>
2025-12-02 16:38:23 +0100

    initial package and failing board tests

---
+++ Sources/Connect4/Connect4Board.swift
@@ -0,0 +1,3 @@
+struct Connect4Board {
+
+}
--- Tests/Connect4Tests/Connect4Tests.swift
+++ Tests/Connect4Tests/Connect4Tests.swift
@@ -2,11 +2,10 @@ import XCTest
 @testable import Connect4
 
 final class Connect4Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testShouldCreateABoardWithSixRowsAndSevenColumns() {
+        let board = Connect4Board()
+        XCTFail("Not implemented: dimensions test")
+        _ = board
     }
 }
