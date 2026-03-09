ff1c3c82999812a6fc6a05685e62454e6336ab27
Dheeraj Chahar <dheeraj.chahar@ns.nl>
2026-03-09 18:39:16 +0100

    Test ascending diagonal win with 4 coins

--- Tests/Connect4Tests/GameWinDetectionTests.swift
+++ Tests/Connect4Tests/GameWinDetectionTests.swift
@@ -58,4 +58,19 @@ final class GameWinDetectionTests: XCTestCase {
         let board = Connect4Board()
         XCTAssertFalse(board.hasDiagonalWin(board.player1Coin))
     }
+
+    func testAscendingDiagonalWinWithFourCoins() {
+        var board = Connect4Board()
+        board.dropCoin(column: 1, coin: board.player1Coin)
+        board.dropCoin(column: 2, coin: board.player2Coin)
+        board.dropCoin(column: 2, coin: board.player1Coin)
+        board.dropCoin(column: 3, coin: board.player2Coin)
+        board.dropCoin(column: 3, coin: board.player2Coin)
+        board.dropCoin(column: 3, coin: board.player1Coin)
+        board.dropCoin(column: 4, coin: board.player2Coin)
+        board.dropCoin(column: 4, coin: board.player2Coin)
+        board.dropCoin(column: 4, coin: board.player2Coin)
+        board.dropCoin(column: 4, coin: board.player1Coin)
+        XCTAssertTrue(board.hasDiagonalWin(board.player1Coin))
+    }
 }
